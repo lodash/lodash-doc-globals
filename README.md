c96162da94c5f8136f682c847c17af3fb53a3706
John-David Dalton <john.david.dalton@gmail.com>
2016-07-30 19:09:57 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# lodash-doc-globals v0.1.0
+
+Lodash documentation globals exported for
+[markdown-doctest](https://www.npmjs.com/package/markdown-doctest) & [tonic](https://tonicdev.com/).
---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var each = require('lodash/each'),
+    noop = require('lodash/noop');
+
+function mockQuery() {
+  return {
+    'on': function(eventName, callback) {
+      callback();
+    }
+  };
+}
+
+mockQuery.each = each;
+
+module.exports = {
+  // Example mocks.
+  'asyncSave': noop,
+  'addContactToList': noop,
+  'batchLog': noop,
+  'calculateLayout': noop,
+  'createApplication': noop,
+  'data': { 'user': 'mock' },
+  'mainText': '',
+  'renewToken': noop,
+  'sendMail': noop,
+  'updatePosition': noop,
+
+  // DOM mocks.
+  'document': { 'body': { 'childNodes': [], 'nodeName': 'BODY' } },
+  'element': {},
+  'EventSource': noop,
+  'jQuery': mockQuery,
+  'window': {},
+
+  // Node.js mocks.
+  'Buffer': Buffer,
+  'fs': { 'writeFileSync': noop },
+  'path': require('path'),
+  'process': process,
+  'setImmediate': setImmediate,
+
+  // Lodash v3.10.1 mocks.
+  'mage': { 'castSpell': noop },
+  'models': { 'todo': {} },
+  'throttled': { 'cancel': noop }
+};
